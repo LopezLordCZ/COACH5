c189ae8785ad69b59124a752d3b54e822a41c07d
Djordje <djordje.chat@gmail.com>
2022-03-30 00:50:38 +0200

    Location services added

--- app/src/main/java/com/example/coach5/MainActivity.java
+++ app/src/main/java/com/example/coach5/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.coach5;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Build;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,7 +31,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         register.setOnClickListener(this);
         login = (Button) findViewById(R.id.login);
         login.setOnClickListener(this);
+
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                //request Location Permission
+                startService();
+            }
+        }
+        else{
+            //Start the Location Service
+            startService();
+        }
+
     }
+
+    void startService() {
+        Intent intent = new Intent(MainActivity.this, LocationService.class);
+        startService(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startService();
+                } else {
+                    Toast.makeText(this, "Permissions required", Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+
 
     @Override
     public void onClick(View v) {
@@ -37,4 +77,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
+
 }
